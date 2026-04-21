6bd16fa2e242abaaa52b5d557a6ebd1cbefbab32
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-04-21 15:57:08 +0600

    Create director's class

---
+++ src/Director.java
@@ -0,0 +1,17 @@
+public class Director {
+
+    private String fullName;
+
+    public Director(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public String toString() {
+        return fullName;
+    }
+}
