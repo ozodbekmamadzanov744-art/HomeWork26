35287dc71be7de0951fa6a8587eac5009210df3b
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-04-21 16:29:13 +0600

    App run

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Movie> allMovies = JsonReader.loadMovies("src/data/movies.json");
+
+        if (allMovies != null) {
+            MovieApp app = new MovieApp(allMovies);
+            app.start();
+        }
     }
 }
